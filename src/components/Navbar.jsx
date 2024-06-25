46d21bd43e8b3f1cea9ed8101af3861eb83c5d73
Michael Harlow <michaelharlow1@outlook.com>
2024-06-25 00:09:12 -0500

    navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,23 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="max-w-7xl m-auto">
-      <nav className="flex justify-between items-center box-border p-4 m-4 rounded-2xl bg-gray-800 text-white sticky top-4">
+      <nav className="flex justify-between items-center box-border p-4 m-4 rounded-2xl border-black border-4 bg-slate-50 text-black sticky top-4">
         <h1 className="text-2xl font-bold">@Harlow</h1>
         <ul className="flex space-x-4">
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/" className="font-bold text-lg">
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <Link href="/about" className="font-bold text-lg">
+              About
+            </Link>
           </li>
           <li>
-            <Link href="/projects">Projects</Link>
+            <Link href="/projects" className="font-bold text-lg">
+              Projects
+            </Link>
           </li>
         </ul>
       </nav>
