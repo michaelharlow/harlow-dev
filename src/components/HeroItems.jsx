45b7a355ea0a00094b93b845c04a03c363bf7f8c
Michael Harlow <michaelharlow1@outlook.com>
2024-06-30 22:34:22 -0500

    mobile nav

---
+++ src/components/HeroItems.jsx
@@ -0,0 +1,11 @@
+export default function HeroItems() {
+  return (
+    <div>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="w-56 h-36 bg-lime-500"></div>
+        <div className="w-56 h-36 bg-teal-500"></div>
+        <div className="w-56 h-36 bg-purple-500"></div>
+      </div>
+    </div>
+  );
+}
