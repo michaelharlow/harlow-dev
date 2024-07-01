f17b09420f53fe42398dd2986b884c4ef8ef16c9
Michael Harlow <michaelharlow1@outlook.com>
2024-06-30 23:42:03 -0500

    micro-interactions

--- src/components/HeroItems.jsx
+++ src/components/HeroItems.jsx
@@ -2,9 +2,9 @@ export default function HeroItems() {
   return (
     <div>
       <div className="grid grid-cols-3 gap-4">
-        <div className="w-56 h-36 bg-lime-500"></div>
-        <div className="w-56 h-36 bg-teal-500"></div>
-        <div className="w-56 h-36 bg-purple-500"></div>
+        <div className="w-56 h-36 rounded-xl bg-lime-500"></div>
+        <div className="w-56 h-36 rounded-xl bg-teal-500"></div>
+        <div className="w-56 h-36 rounded-xl bg-purple-500"></div>
       </div>
     </div>
   );
--- src/components/HeroName.jsx
+++ src/components/HeroName.jsx
@@ -1,21 +1,26 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 export default function HeroName() {
   return (
-    <div className="">
+    <motion.div>
       <h1 className="md:text-8xl xl:text-9xl text-7xl font-bold text-center">
         <SplitLetters text="Michael" />
         <span className="sm:inline-block hidden">&nbsp;</span>
         <SplitLetters text="Harlow" />
       </h1>
-      <p className="sm:text-xl m-auto max-w-prose sm:text-center sm:px-0 px-4">
-        Hello, I am a software engineer and student. I am passionate about
-        creating exciting and engaging projects, learning new things, and
-        sharing my knowledge with my community.
-      </p>
-    </div>
+      <motion.p
+        initial={{ opacity: 0, scale: 0.9, y: 20 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{ delay: 1, ease: "easeOut" }}
+        className="sm:text-xl mx-auto my-4 max-w-prose sm:text-center xl:text-wrap text-balance sm:px-0 px-4"
+      >
+        <motion.span>Hello</motion.span>, I am a software engineer and student.
+        I am passionate about creating exciting and engaging projects, learning
+        new things, and sharing my knowledge with my community.
+      </motion.p>
+    </motion.div>
   );
 }
 
@@ -40,11 +45,16 @@ function SplitLetters({ text }) {
       {text.split("").map((letter, index) => (
         <motion.span
           className={"inline-block"}
-          key={index} // TODO: key could be index combined with letter to be more unique?
+          key={index + letter} // TODO: key could be index combined with letter to be more unique?
           initial="begin"
           animate="end"
+          whileHover={{
+            scale: 1.2,
+            rotate: Math.random() * 10 - 5,
+            transition: { duration: 0.1 },
+          }}
           variants={defaultAnimation}
-          transition={{ delay: index * Math.random() * 0.2 }}
+          transition={{ delay: index * Math.random() * 0.1 }}
         >
           {letter}
         </motion.span>
