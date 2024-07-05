29fa58b7e7baa3b7158a535d222391def5eea70b
Michael Harlow <michaelharlow1@outlook.com>
2024-07-05 00:26:37 -0500

    minimal hero + links

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import HeroItems from "@/components/HeroItems";
 import HeroName from "@/components/HeroName";
-import Shapes from "@/components/Shapes";
 
 export default function Home() {
   return (
@@ -8,7 +7,21 @@ export default function Home() {
       <div className="m-auto max-w-7xl">
         <main className="flex flex-col justify-center items-center h-[80vh] w-full">
           <HeroName />
-          <HeroItems />
+          <p>
+            <a
+              className="hover:underline"
+              href="https://github.com/michaelharlow"
+            >
+              Github
+            </a>
+            {" • "}
+            <a
+              className="hover:underline"
+              href="https://www.linkedin.com/in/michaelaharlow/"
+            >
+              linkedin
+            </a>
+          </p>
         </main>
       </div>
     </>
