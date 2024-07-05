7f813adad75c936fe7abe149b7d7eee7e2cec621
Michael Harlow <michaelharlow1@outlook.com>
2024-07-05 00:29:12 -0500

    create route

---
+++ src/app/projects/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-[80vh] flex justify-center items-center">
+      <p className="">
+        Would you believe this it is a project to finish this landing page?
+      </p>
+    </main>
+  );
+}
