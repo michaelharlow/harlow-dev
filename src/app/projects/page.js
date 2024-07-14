1918baa2afb29c4e88c6003cc119b50b6b57bbc9
Michael Harlow <michaelharlow1@outlook.com>
2024-07-14 00:09:29 -0500

    content

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,9 +1,22 @@
-export default function Home() {
+import Link from "next/link";
+
+export default function Page() {
   return (
-    <main className="h-[80vh] flex justify-center items-center">
-      <p className="">
-        Would you believe this it is a project to finish this landing page?
-      </p>
+    <main className="m-auto max-w-3xl sm:px-0 px-6">
+      <h1 className="sm:text-5xl text-2xl sm:leading-snug font-semibold my-12 text-left">
+        /Projects
+      </h1>
+      <ul className="space-y-3">
+        <li className="hover:underline hover:bg-amber-100 p-4 pl-1">
+          <Link
+            href="https://trandrew1023.github.io/edi-translator/"
+            prefetch={false}
+          >
+            <h2 className="text-xl font-bold">EDI Translator</h2>
+            <p>X12 EDI file translator web app</p>
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
